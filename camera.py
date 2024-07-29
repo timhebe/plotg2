b3cfa611bfbe17f4ccbf653b2d9725d0a4335633
thebens <tim.hebenstreit@mpl.mpg.de>
2024-07-29 13:16:39 +0200

    debugging

--- camera.py
+++ camera.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+import plotly.express as px
 from cryo import CamReader
 import io
 
@@ -11,14 +12,22 @@ def plot_cam(file):
         name = file.name.split('.')[0]
         st.write(name)
 
-    PSF = CamReader(name+'.cam', True)
-    plt.imshow(PSF[0])
+    CAM = CamReader(name+'.cam', True)
+
+    """
+    plt.imshow(CAM[0])
     plt.title("Cryo Measurement")
     st.pyplot()
-
+    
     buf = io.BytesIO()
     plt.savefig(buf, format="pdf")
     st.download_button("Download as PDF", buf.getvalue(), file_name=f"{name}.pdf")
     buf.seek(0)
     plt.savefig(buf, format="png")
     st.download_button("Download as PNG", buf.getvalue(), file_name=f"{name}.png")
+    """
+
+    # new way of plotting
+    labels = {'x': "X Axis Title", 'y': "X Axis Title", 'color': 'Z Label'}
+    fig = px.imshow(CAM[0], labels=labels)  # aspect='equal'
+    st.plotly_chart(fig)
