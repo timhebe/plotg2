7266b7249ef069c1ca907293c4c5ec2f04041177
thebens <tim.hebenstreit@mpl.mpg.de>
2024-07-29 16:44:07 +0200

    readded fitting options

--- g2.py
+++ g2.py
@@ -9,6 +9,16 @@ def g2_function(x, p, q, y0, x0, a):
     return y0 + a * (1 - np.exp(-0.5 * p * np.abs(x-x0)) * (np.cos(0.5 * q * np.abs(x-x0)) + p/q * np.sin(0.5 * q * np.abs(x-x0))))
 
 def plot_g2(file, device):
+    with st.sidebar:
+        moleculeTitle = st.text_input("Molecule Title", value="")
+        initial_p = st.number_input("Initial p value", value=0.4)
+        initial_q = st.number_input("Initial q value", value=0.1)
+        initial_y0 = st.number_input("Initial y0 value", value=500)
+        initial_x0 = st.number_input("Initial x0 value", value=0)
+        initial_a = st.number_input("Initial a value", value=5e4)
+        initial_guess = (initial_p, initial_q, initial_y0, initial_x0, initial_a)
+        printInfo = st.checkbox("Print fitting info", value=False)
+
     if isinstance(file, str):  # Demo mode
         name = file.split('/')[-1].split('.')[0]
         data = pd.read_csv(file, delimiter='\t', header=0)
@@ -28,12 +38,15 @@ def plot_g2(file, device):
         x = data["Time_ns"]
         y = data["G_t"]
 
-    initial_guess = (0.4, 0.1, 500, 0, 1000)
     popt, _ = curve_fit(g2_function, x, y, p0=initial_guess)
     y0 = popt[2]
     a = popt[4]
     g_2_0 = g2_function(0, *popt) / (y0 + a)
     textstr = r'$g^{(2)} (\tau = 0) = $' + str(round(g_2_0 * 100, 1)) + '%'
+
+    if printInfo:
+        st.write("Optimal parameters (p, q, y0, x0, a): ", popt)
+        st.write(r'$g^{(2)} (\tau = 0) = $' + str(round(g_2_0 * 100, 1)) + '%')
 
     plt.figure()
     plt.plot(x, y, label="Data")
