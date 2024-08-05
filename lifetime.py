3ad871cb2f75cd51c0dfe829232cead8ac639b20
thebens <tim.hebenstreit@mpl.mpg.de>
2024-08-05 16:23:35 +0200

    debugging

--- lifetime.py
+++ lifetime.py
@@ -52,27 +52,6 @@ def plot_lifetime(file, device):
         x = data["Time_ns"]
         y = data["Counts_per_bin"]
 
-    # Peak finding
-    peaks, properties = find_peaks(y, prominence=np.max(y) / 2)
-    x_pk = x[peaks]
-    data_pk = y[peaks]
-
-    to_delete = np.where(data_pk < max(data_pk) / 100)
-    data_pk = np.delete(data_pk, to_delete)
-    x_pk = np.delete(x_pk, to_delete)
-
-    first_peak = x_pk[0]  # in ns
-
-    # Start and stop in [ns]
-    # We will fit between start and stop only. These parameters influence the fit a lot.
-    # Start at the first peak + 1 ns, stop 150 ns later.
-    start = st.sidebar.number_input('Start (in ns)', 0.0, float(max(y)), first_peak + 1)
-    stop = st.sidebar.number_input('Stop (in ns)', 0.0, float(max(y)), first_peak + 151)
-    Y_fit = y[int(start):int(stop)]
-    X_fit = np.arange(0, len(Y_fit))
-
-    st.write(start, stop)
-
     plt.figure()
     plt.plot(x, y, label="Data")
     plt.xlabel("Time (ns)")
@@ -80,6 +59,28 @@ def plot_lifetime(file, device):
     plt.title(f"Lifetime Measurement ({device})")
     if log_scale:
         plt.yscale('log')
+
+    if fit_type != "None":
+        # Peak finding
+        peaks, properties = find_peaks(y, prominence=np.max(y) / 2)
+        x_pk = x[peaks]
+        data_pk = y[peaks]
+
+        to_delete = np.where(data_pk < max(data_pk) / 100)
+        data_pk = np.delete(data_pk, to_delete)
+        x_pk = np.delete(x_pk, to_delete)
+
+        first_peak = x_pk[0]  # in ns
+
+        # Start and stop in [ns]
+        # We will fit between start and stop only. These parameters influence the fit a lot.
+        # Start at the first peak + 1 ns, stop 150 ns later.
+        start = st.sidebar.number_input('Start (in ns)', 0.0, float(max(y)), first_peak + 1)
+        stop = st.sidebar.number_input('Stop (in ns)', 0.0, float(max(y)), first_peak + 151)
+        Y_fit = y[int(start):int(stop)]
+        X_fit = np.arange(0, len(Y_fit))
+
+        st.write(start, stop)
 
     if fit_type == "Single Exponential":
         # Sidebar for single exponential fitting parameters
